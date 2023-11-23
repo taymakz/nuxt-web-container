44dcbd681b2e0431816c36aedebead40f3092089
TaymaZ <taymazak1382@gmail.com>
2023-11-23 22:20:22 +0330

    chore: setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@vueuse/nuxt", "@unocss/nuxt", "@nuxt/content", "@nuxtjs/color-mode"],
-  
-  colorMode:{
-    classSuffix:'',
+  modules: ['@vueuse/nuxt', '@unocss/nuxt', '@nuxt/content', '@nuxtjs/color-mode'],
+
+  colorMode: {
+    classSuffix: '',
 
   },
-  css:[
+  css: [
     '@unocss/reset/tailwind.css',
     '~/assets/css/fonts.css',
     '~/assets/css/main.css',
-  ]
+  ],
 })
